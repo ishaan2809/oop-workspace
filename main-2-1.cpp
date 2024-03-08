22a6a6aca19ca243a01c26afc677ebe0bc68fd03
ishaan2809 <ishaan2809@gmail.com>
2024-03-08 16:22:49 +1030

    finished

---
+++ main-2-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+extern void print_binary_str(std::string decimal_number);
+
+int main(){
+    std::string decimal_number = "45";
+    print_binary_str(decimal_number);
+    
+    return 0;
+    
+
+}
