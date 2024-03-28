3460f060c1f7f1d6b7b528d8d6322b328c5efb49
ishaan2809 <ishaan2809@gmail.com>
2024-03-28 11:48:41 +1030

    pracexam-3

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -3,7 +3,7 @@
 int main() {
 Unit u1;
 std::cout << u1.get_Num_Bedrooms() << u1.get_Value() << u1.get_Area()<< std::endl;
-Unit u2(4, 5, 6);
+Unit u2(3, 27000, 120);
 std::cout << u2.get_Num_Bedrooms() <<u2.get_Value() << u2.get_Area() << std::endl;
 return 0;
 }
