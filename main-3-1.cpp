74dcef9e7da8ccfbb59b7cf6a19ab491cac39fc6
ishaan2809 <ishaan2809@gmail.com>
2024-04-28 12:47:41 +0930

    asdf

--- main-3-1.cpp
+++ main-3-1.cpp
@@ -13,40 +13,4 @@ int main(){
     TV tv1(465,88.5);
     Fridge f2(2,2.2);
     TV tv2(10,8.5);
-    
-
-    if (h1.addAppliance(&f1))
-    {
-        std::cout << "appliance added" << std::endl;
-    }else{
-        std::cout << "appliance not added" << std::endl;
-    }
-
-    if (h1.addAppliance(&f2))
-    {
-        std::cout << "appliance added" << std::endl;
-    }else{
-        std::cout << "appliance not added" << std::endl;
-    }
-
-    if (h1.addAppliance(&tv1))
-    {
-        std::cout << "appliance added" << std::endl;
-    }else{
-        std::cout << "appliance not added" << std::endl;
-    }
-
-    if (h1.addAppliance(&tv2))
-    {
-        std::cout << "appliance added" << std::endl;
-    }else{
-        std::cout << "appliance not added" << std::endl;
-    }
-
-    std::cout << "Total power consumption:" << h1.getTotalPowerConsumption() << std::endl;
-
-
-    
-
-    return 0;
 }
